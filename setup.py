83f6b5555e541d2add0ad89c2a3828f2e14c41e2
Luca Scotzniovsky <lucascotzniovsky@Lucas-MacBook-Pro.local>
2020-07-23 15:16:53 -0700

    test2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='fenics_motor_model',
+    version='1',
+    packages=[
+        'fenics_motor_model',
+    ],
+    install_requires=[
+    ],
+)
