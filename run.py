737f66d0181ae8f512638c7b5b034b2a2d72ae3f
Luca Scotzniovsky <lucascotzniovsky@Lucas-MacBook-Pro.local>
2020-07-24 00:43:43 -0700

    update

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-'''This repository models a Permanent Magnet Synchronous Motor (PMSM) 
+'''This file models a Permanent Magnet Synchronous Motor (PMSM) 
 for EVTOL aircrafts'''
 
 import numpy as np
